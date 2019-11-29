91387b7d2faf6fe569f33a53872a059831abaa96
Eva Mutuku <mutileeva@gmail.com>
2019-11-29 06:36:06 -0800

    Connect forms on index file to JS script.

---
+++ script.js
@@ -0,0 +1,29 @@
+// var DD = parseInt(prompt("Enter the day you were born"));
+
+
+// if (DD<=0 || DD>31){
+// alert("Invalid Day!");
+// prompt("Please enter the correct day")
+// }
+// else {
+// var MM = prompt("Enter the month you were born "+1+".January "+2+".February "+3+".March "+4+".April "+5+".May "+6+".June "+7+".July "+8+".August "+9+".September "+10+".October "+11+".November "+12+"December")
+
+// if (MM<=0 || MM>12){
+// alert("Invalid month!");
+// prompt("Please enter the correct month.")
+// }
+// else {
+// var dayOfBirth = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+// }
+// }
+
+
+function submit(){
+  // var fname = document.forms["form_reg"]["firstname"].value;
+  // alert(fname);
+  // var sex = document.getElementById("form_reg");
+  // var gender = sex.option[sex.selecedIndex].text;
+  // alert(fname);
+  // alert(gender);
+  alert("Hello")
+}
