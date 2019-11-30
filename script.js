515afab09172b9cf641c7681284a13afd6c2f707
Eva Mutuku <mutileeva@gmail.com>
2019-11-30 13:49:57 -0800

    Make a few changes to the functions on script.

--- script.js
+++ script.js
@@ -35,46 +35,46 @@ if (monthOfBirth<=0 || monthOfBirth>12){
 var CC = birthYear.slice(0,2);
 var YY = birthYear.slice(2,4);
 // Calculating the day one was born
-var dayOfBirth =  parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(monthOfBirth+1)/10)) + birthDate )%7);
+var dayOfBirth = parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(monthOfBirth+1)/10)) + birthDate )%7);
+var birthDay = dayOfBirth.toString();
 
 // Giving names depending on gender and day of birth
 var akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var akanMaleNames = ["Kwasi", "Kwacho", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-if (sex==female){
-  if(dayOfBirth==1){
-    alert("Your Akan name is "+akanFemaleNames[0]+". This means that you were born on Sunday");
-  } else if(dayOfBirth==2){
-    alert("Your Akan name is "+akanFemaleNames[1]+". This means that you were born on Monday");
-  }else if(dayOfBirth==3){
-    alert("Your Akan name is "+akanFemaleNames[2]+". This means that you were born on Tuesday");
-  }else if(dayOfBirth==4){
-    alert("Your Akan name is "+akanFemaleNames[3]+". This means that you were born on Wednesday");
-  }else if(dayOfBirth==5){
-    alert("Your Akan name is "+akanFemaleNames[4]+". This means that you were born on Thursday");
-  }else if(dayOfBirth==6){
-    alert("Your Akan name is "+akanFemaleNames[5]+". This means that you were born on Friday");
-  }else{
-    alert("Your Akan name is "+akanFemaleNames[6]+". This means that you were born on Saturday");
-  }
+if (sex=="female" && birthday == "1"){
+  document.write("Your Akan name is "+akanFemaleNames[0]+". This means that you were born on Sunday");
+  // alert("Your Akan name is "+akanFemaleNames[0]+". This means that you were born on Sunday");
+} else if(sex=="female" && birthDay=="2"){
+  alert("Your Akan name is "+akanFemaleNames[1]+". This means that you were born on Monday");
+}else if(sex=="female" && birthDay=="3"){
+  alert("Your Akan name is "+akanFemaleNames[2]+". This means that you were born on Tuesday");
+}else if(sex=="female" && birthDay=="4"){
+  alert("Your Akan name is "+akanFemaleNames[3]+". This means that you were born on Wednesday");
+}else if(sex=="female" && birthDay=="5"){
+  alert("Your Akan name is "+akanFemaleNames[4]+". This means that you were born on Thursday");
+}else if(sex=="female" && birthDay=="6"){
+  alert("Your Akan name is "+akanFemaleNames[5]+". This means that you were born on Friday");
+}else{
+  alert("Your Akan name is "+akanFemaleNames[6]+". This means that you were born on Saturday");
 }
-else {
-  if(dayOfBirth==1){
-    alert("Your Akan name is "+akanMaleNames[0]+". This means that you were born on Sunday");
-  } else if(dayOfBirth==2){
-    alert("Your Akan name is "+akanMaleNames[1]+". This means that you were born on Monday");
-  }else if(dayOfBirth==3){
-    alert("Your Akan name is "+akanMaleNames[2]+". This means that you were born on Tuesday");
-  }else if(dayOfBirth==4){
-    alert("Your Akan name is "+akanMaleNames[3]+". This means that you were born on Wednesday");
-  }else if(dayOfBirth==5){
-    alert("Your Akan name is "+akanMaleNames[4]+". This means that you were born on Thursday");
-  }else if(dayOfBirth==6){
-    alert("Your Akan name is "+akanMaleNames[5]+". This means that you were born on Friday");
-  }else{
-    alert("Your Akan name is "+akanMaleNames[6]+". This means that you were born on Saturday");
-  }
+if(sex=="male" && birthDay=="1"){
+  alert("Your Akan name is "+akanMaleNames[0]+". This means that you were born on Sunday");
+} else if(sex=="male" &&birthDay=="2"){
+  alert("Your Akan name is "+akanMaleNames[1]+". This means that you were born on Monday");
+}else if(sex=="male" &&birthDay=="3"){
+  alert("Your Akan name is "+akanMaleNames[2]+". This means that you were born on Tuesday");
+}else if(sex=="male" &&birthDay=="4"){
+  alert("Your Akan name is "+akanMaleNames[3]+". This means that you were born on Wednesday");
+}else if(sex=="male" &&birthDay=="5"){
+  alert("Your Akan name is "+akanMaleNames[4]+". This means that you were born on Thursday");
+}else if(sex=="male" &&birthDay=="6"){
+  alert("Your Akan name is "+akanMaleNames[5]+". This means that you were born on Friday");
+}else{
+  alert(sex=="male" &&"Your Akan name is "+akanMaleNames[6]+". This means that you were born on Saturday");
 }
+
+
 
 
 
